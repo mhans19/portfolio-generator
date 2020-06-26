f4a172dd0a209ed3d7fbebb400e421d5c0d2aea2
mhans19 <mdhansen19@gmail.com>
2020-06-25 21:44:04 -0600

    created modularized html and dynamic input

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  };
+const profileDataArgs = process.argv.slice(2);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
