32a3ad5c6ea04087258932512b61153485f1618e
mhans19 <mdhansen19@gmail.com>
2020-06-26 00:00:31 -0600

    added validation logic

--- app.js
+++ app.js
@@ -10,12 +10,28 @@
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                return true;
+                } else {
+                console.log('Please enter your name!');
+                return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                return true;
+                } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -40,12 +56,28 @@
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                return true;
+                } else {
+                console.log('Please enter your project name!');
+                return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                return true;
+                } else {
+                console.log('Please enter your project description!');
+                return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -56,7 +88,15 @@
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLinkInput => {
+                if (githubLinkInput) {
+                return true;
+                } else {
+                console.log('Please enter your project GitHub link!');
+                return false;
+                }
+            }
         },
         {
             type: 'confirm',
@@ -80,7 +120,7 @@
             }
           });
     };    
-// Promise logis for prompts
+// Promise logic for prompts
     promptUser()
     .then(promptProject)
     .then(portfolioData => {
