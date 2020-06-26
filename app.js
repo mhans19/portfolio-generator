01846d03b4e98f65296e69a4f9246608c40c839e
mhans19 <mdhansen19@gmail.com>
2020-06-25 23:04:17 -0600

    npm set up with inquirer question object

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// NPA Modules
+    const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// Veryify Module is successfully imported
+    //console.log(inquirer);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ])
+    .then(answers => console.log(answers));    
